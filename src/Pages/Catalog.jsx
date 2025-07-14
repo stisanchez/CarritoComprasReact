158127865a080659ae43fe95e2c39ffb2a6783a6
Stiven Sanchez <stsanchez92@gmail.com>
2025-07-14 15:06:26 -0600

    Se corrige card de catalogo

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -4,33 +4,20 @@ import { ImagenComponent } from './../Components/Common/ImagenComponent';
 import { Link } from 'react-router-dom';
 import { Button } from 'primereact/button';
 import DataContext from '../Components/Context/DataContext';
+import CardProductsBig from '../Components/Products/CardProductsBig';
 
 export const Catalog = () => {
   const { products } = GetAll_Products();
-  const { setCarrito_Function } = useContext(DataContext);
-
   return (
     <>
       <div className='contenedor-products-list'>
         {
           products.map(product => (
-            <div key={product.id} className='contenedor-cards'>
 
-              <div className="card-product-catalog">
-                <div className='contenedor-imagen-catalog'>
-                  <ImagenComponent ruta={product.image} className={'imagen-component'}></ImagenComponent>
-                </div>
-                <div className="card-product-catalog-body">
-                  <h2 className="card-product-catalog-title">{product.title.slice(0, 60)}</h2>
-                  <p className="card-product-catalog-text">{product.description}</p>
-
-                  <div className='contenedor-boton-agregar-carrito'>
-                    <Button label="Agregar a carrito" icon="pi pi-cart-plus" iconPos="right" onClick={()=> setCarrito_Function(product)}/>
-                  </div>
-
-                </div>
-              </div>
-            </div>
+              <CardProductsBig 
+                key={product.id}
+                product = {product}
+              />
           ))
         }
       </div>
